b76bbd58a06f7c38e265800ecacea9898cd8d413
pablo <pablo.webdevlpr@gmail.com>
2021-09-29 04:32:23 -0500

    14: BTN UP REDUX

--- js/up.js
+++ js/up.js
@@ -5,7 +5,7 @@ window.onscroll = function (){
         document.querySelector('.go-top-cont').classList.remove('show');
     }
 
-    if(document.documentElement.scrollTop > 2700){
+    if(document.documentElement.scrollTop > 2500){
         document.querySelector('.go-top-cont').classList.remove('show');
     }
 }
